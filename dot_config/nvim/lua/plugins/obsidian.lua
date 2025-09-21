609f966918e10802d96f4747b6bfc3e020adedf1
e-mar404 <egmarinn@gmail.com>
2025-09-20 21:29:48 -0500

    nvim: reformat obsidian

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,45 +1,43 @@
 return {
-  {
-    "obsidian-nvim/obsidian.nvim",
-    -- dir = "~/plugins/obsidian.nvim",
-    -- dev = { true },
-    lazy = false,
+  "obsidian-nvim/obsidian.nvim",
+  -- dir = "~/plugins/obsidian.nvim",
+  -- dev = { true },
+  lazy = false,
 
-    event = {
-      "BufReadPre /home/emar/obsidian/*/*.md",
+  event = {
+    "BufReadPre /home/emar/obsidian/*/*.md",
+  },
+
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false,
+
+    workspaces = {
+      {
+        name = "obsidian",
+        path = "~/obsidian",
+      },
+    },
+    daily_notes = {
+      folder = "900-daily-notes",
+      default_tags = { "daily-notes" },
+      template = "999-templates/daily-notes.md",
     },
 
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+      create_new = true,
     },
 
-    ---@module 'obsidian'
-    ---@type obsidian.config
-    opts = {
-      legacy_commands = false,
-
-      workspaces = {
-        {
-          name = "obsidian",
-          path = "~/obsidian",
-        },
-      },
-      daily_notes = {
-        folder = "900-daily-notes",
-        default_tags = { "daily-notes" },
-        template = "999-templates/daily-notes.md",
-      },
-
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-        create_new = true,
-      },
-
-      templates = {
-        folder = "999-templates",
-      },
-
+    templates = {
+      folder = "999-templates",
     },
+
   },
 }
