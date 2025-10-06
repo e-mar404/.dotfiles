d48ceec367502486a5385af60ff22df92346f694
e-mar404 <egmarinn@gmail.com>
2025-10-06 17:36:41 -0500

    remove plugin update check

--- dot_config/nvim/lua/emar/lazy.lua
+++ dot_config/nvim/lua/emar/lazy.lua
@@ -18,6 +18,5 @@ require("lazy").setup {
   spec = {
     { import = "plugins" },
   },
-  install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+  install = { colorscheme = { "catppuccin" } },
 }
