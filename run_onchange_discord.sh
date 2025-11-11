c1b1e2668135c95a9887f324e289b69b732e1201
e-mar404 <egmarinn@gmail.com>
2025-11-11 00:26:09 -0600

    update discord

--- run_onchange_discord.sh
+++ run_onchange_discord.sh
@@ -5,9 +5,9 @@ sudo rm -rf /opt/Discord
 sudo rm /usr/bin/Discord
 sudo rm /usr/share/applications/discord.desktop
 
-curl -O "https://stable.dl2.discordapp.net/apps/linux/0.0.112/discord-0.0.112.tar.gz"
+curl -O "https://stable.dl2.discordapp.net/apps/linux/0.0.113/discord-0.0.113.tar.gz"
 
-sudo tar -zxvf "discord-0.0.112.tar.gz" -C /opt
+sudo tar -zxvf "discord-0.0.113.tar.gz" -C /opt
 
 sudo ln -sf -v /opt/Discord/Discord /usr/bin/Discord
 
