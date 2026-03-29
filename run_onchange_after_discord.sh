462a9f88894c7fa4067edbb741b07b299f8b6103
e-mar404 <egmarinn@gmail.com>
2026-03-29 11:39:36 -0500

    manually install discord

---
+++ run_onchange_after_discord.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+# delete old discord stuff
+sudo rm -rf /opt/Discord
+sudo rm /usr/share/discord/Discord
+sudo rm /usr/share/applications/discord.desktop
+sudo rm /usr/share/icons/hicolor/256x256/apps/discord.png
+
+curl -O "https://stable.dl2.discordapp.net/apps/linux/0.0.130/discord-0.0.130.tar.gz"
+
+sudo tar -zxvf "discord-0.0.130.tar.gz" -C /opt
+
+sudo mkdir -p /usr/share/discord
+sudo ln -sfv /opt/Discord/Discord /usr/share/discord/Discord
+
+sudo cp -rv /opt/Discord/discord.desktop /usr/share/applications
+sudo cp -v /opt/Discord/discord.png /usr/share/icons/hicolor/256x256/apps/discord.png
+
+rm -rf *.tar.gz
