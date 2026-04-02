70b460aa65006b6ad02cb2e649c488814d72901a
e-mar404 <egmarinn@gmail.com>
2026-04-02 15:08:56 -0500

    [script] ask if pkgs script should run or not
    i dont want to install/update pkgs every time that I run chezmoi apply,
    but i dont want it to be just after a change

--- run_after_pkgs.sh
+++ run_after_pkgs.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
 
+validInput=false
+while [[ $validInput != true ]] ; do
+  read -n 1 -p 'Run pkgs.sh [y|n] ' run 
+  if [[ $run == 'n' ]]; then
+    exit 0
+  elif [[ $run == y ]]; then
+    validInput=true
+  else 
+    printf "\nthat is an invalid option\n"
+  fi
+done
+
 pkgs_file='./packages.txt'
 pkgs=''
 while read package; do
