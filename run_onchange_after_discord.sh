6766592297cb34e9cffa25f65d364c7b33cc86a9
e-mar404 <egmarinn@gmail.com>
2026-04-11 22:46:08 -0500

    [fix] wayland things

--- run_onchange_after_discord.sh
+++ run_onchange_after_discord.sh
@@ -6,9 +6,9 @@ sudo rm /usr/share/discord/Discord
 sudo rm /usr/share/applications/discord.desktop
 sudo rm /usr/share/icons/hicolor/256x256/apps/discord.png
 
-curl -O "https://stable.dl2.discordapp.net/apps/linux/0.0.130/discord-0.0.130.tar.gz"
+curl -O "https://stable.dl2.discordapp.net/apps/linux/0.0.132/discord-0.0.132.tar.gz"
 
-sudo tar -zxvf "discord-0.0.130.tar.gz" -C /opt
+sudo tar -zxvf "discord-0.0.132.tar.gz" -C /opt
 
 sudo mkdir -p /usr/share/discord
 sudo ln -sfv /opt/Discord/Discord /usr/share/discord/Discord
