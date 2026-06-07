a8ded770aa3c77ee28898d953dd62ab0db8e27f4
e-mar404 <egmarinn@gmail.com>
2026-06-07 00:31:50 -0500

    [hyrpland]: go back to default active border

--- dot_config/exact_hypr/hyprland.lua
+++ dot_config/exact_hypr/hyprland.lua
@@ -92,7 +92,7 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = "#d6988c",
+            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
             inactive_border = "#515745",
         },
 
